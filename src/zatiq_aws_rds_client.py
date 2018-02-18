6f614d3253bc90984409a47bcac8504fb86739c6
Hasan Jafri <hasan.jafri7@gmail.com>
2018-02-18 00:38:58 -0500

    Zatiq AWS clients to interact with different services

---
+++ src/zatiq_aws_rds_client.py
@@ -0,0 +1,52 @@
+import MySQLdb
+
+class ZatiqAWSRDSClient(object):
+    def __init__(self):
+        self.connect_to_rds = MySQLdb.connect(
+            
+        )
+    
+    def get_all_businesses(self):
+        db_query = self.connect_to_rds.cursor()
+        db_query.execute("""SELECT * from ZatiqBusinesses""")
+        response = db_query.fetchall()
+        print(response)
+        return("Done!")
+
+    def business_login(self, businessEmail, businessPassword):
+        if not businessEmail:
+            return("Please specify your email!")
+        if not businessPassword:
+            return("Please type your password!")
+        
+        db_query = self.connect_to_rds.cursor()
+        db_query.execute("""SELECT * FROM ZatiqBusinesses WHERE businessEmail = %s AND businessPassword = %s""", (businessEmail, businessPassword))
+
+        res = db_query.fetchall()
+
+        if len(res) > 0:
+            logged_in_business = res[0][1]
+            has_set_information = res[0][4]
+            return(logged_in_business, has_set_information)
+        else:
+            return("No such user!")
+    
+    def business_register(self, businessName, businessEmail, businessPassword):
+        if not businessEmail:
+            return("Please specify your email")
+        if not businessPassword:
+            return("Please type in your password")
+        if not businessName:
+            return("Please type in your Business's Name")
+        
+        db_query = self.connect_to_rds.cursor()
+        db_query.execute("""SELECT * FROM ZatiqBusinesses WHERE businessEmail = %s""", (businessEmail))
+
+        res = db_query.fetchall()
+
+        if len(res) > 0:
+            return("Business is already registered with this email")
+        else:
+            register_query = self.connect_to_rds.cursor()
+            register_query.execute(""")
+        
---
+++ src/zatiq_aws_s3_client.py
@@ -0,0 +1,31 @@
+import boto3
+import botocore
+from config import S3_KEY, S3_SECRET, S3_BUCKET
+
+class ZatiqAWSS3Client(object):
+    def __init__(self):
+        self.s3_client = boto3.client("s3", aws_access_key_id=S3_KEY, aws_secret_access_key=S3_SECRET)
+        self.download_s3 = boto3.resource('s3')
+
+    def create_new_s3_bucket(self):
+        self.s3_client.create_bucket(Bucket="zatiq-user-images")
+
+    def get_all_buckets(self):
+        response = self.s3_client.list_buckets()
+        buckets = [bucket['Name'] for bucket in response['Buckets']]
+        return("Bucket List: %s" % buckets)
+    
+    def upload_file_to_s3(self):
+        self.s3_client.upload_file('./sample.jpg', 'zatiq-user-images', 'sample.jpg')
+    
+    def download_file_from_s3(self):
+        try:
+            self.download_s3.Bucket('zatiq-user-images').download_file('sample.jpg', './downloadedfroms3.jpg')
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == "404":
+                print("The file does not exist in the s3 bucket!")
+            else:
+                raise
+
+
+#download_s3.meta.client.download_file("zatiq-user-images", "test.txt", "./downloadedfroms3.txt")
