de7af33e510ad509e4e9afd597c1946aef3c41da
Hasan Jafri <hasan.jafri7@gmail.com>
2018-04-03 01:05:24 -0400

    Updated MySQL Clients to handle business/user register/login functionality

---
+++ src/zatiq_businesses_mysql_client.py
@@ -0,0 +1,67 @@
+import MySQLdb
+from cryptography.fernet import Fernet
+from config import secret_key
+
+class ZatiqBusinessesMySQLClient(object):
+    def __init__(self):
+        self.connect_to_db = MySQLdb.connect(
+        )
+
+    def get_all_businesses(self):
+        db_query = self.connect_to_db.cursor()
+        db_query.execute("""SELECT * from zatiq_businesses""")
+        response = db_query.fetchall()
+        print(response)
+        return("Done!")
+
+    def encrypt_passwd(self, password):
+        encryption = Fernet(secret_key)
+        encrypted_pwd = encryption.encrypt(b"" + str(password))
+        return(bytes(encrypted_pwd).decode("utf-8"))
+    
+    def decode_passwd(self, password):
+       encryption = Fernet(secret_key)
+       encrypted_pwd = password.encode()
+       decrypted_pwd = (encryption.decrypt(encrypted_pwd))
+       return(bytes(decrypted_pwd).decode("utf-8"))
+
+    def business_login(self, businessEmail, businessPassword):
+        if not businessEmail:
+            return("Please specify your email!")
+        if not businessPassword:
+            return("Please type your password!")
+
+        db_query = self.connect_to_db.cursor()
+        db_query.execute("""SELECT * FROM zatiq_businesses WHERE businessEmail = %s AND businessPassword = %s""", (businessEmail, businessPassword))
+
+        response = db_query.fetchall()
+
+        if len(response) > 0:
+            logged_in_business = response[0][3]
+            has_set_information = response[0][4]
+            return(logged_in_business, has_set_information)
+        else:
+            return("No such user!")
+
+    def business_register(self, businessName, businessEmail, businessPassword):
+        if not businessEmail:
+            return("Please specify your email")
+        if not businessPassword:
+            return("Please type in your password")
+        if not businessName:
+            return("Please type in your Business's Name")
+
+        db_query = self.connect_to_db.cursor()
+        db_query.execute("""SELECT * FROM ZatiqBusinesses WHERE businessEmail = %s""", [businessEmail])
+
+        res = db_query.fetchall()
+
+        if len(res) > 0:
+            return("Business is already registered with this email")
+        else:
+            register_query = self.connect_to_db.cursor()
+            #businessInfo = {"businessName": businessName, "businessEmail": businessEmail, "businessPassword": businessPassword}
+            register_query.execute("""INSERT INTO ZatiqBusinesses (businessName, businessEmail, businessPassword) VALUES (%s, %s, %s)""", (
+                businessName, businessEmail, businessPassword))
+            print(register_query.fetchall())
+        
---
+++ src/zatiq_users_mysql_client.py
@@ -0,0 +1,95 @@
+import MySQLdb
+import secrets
+import requests
+
+class ZatiqUsersMySQLClient(object):
+    def __init__(self):
+        self.connect_to_db = MySQLdb.connect(
+            
+        )
+    
+    def get_all_users(self):
+        db_query = self.connect_to_db.cursor()
+        db_query.execute("""SELECT * from zatiq_users""")
+        response = db_query.fetchall()
+        print(response)
+        return("Done!")
+    
+    def generate_zatiq_api_token(self):
+        api_token = secrets.token_urlsafe(32)
+        if self.check_api_token_exists(api_token) == False:
+            return(api_token)
+        else:
+            self.generate_zatiq_api_token()
+
+    def check_api_token_exists(self, api_token):
+        check_api_token = self.connect_to_db.cursor()
+        check_api_token.execute("""SELECT * FROM zatiq_users WHERE zatiq_token = %s""", [api_token])
+        response = check_api_token.fetchall()
+        if len(response) > 0:
+            self.generate_zatiq_api_token()
+        else:
+            return(False)
+
+    def get_user_info(self, authToken, method):
+        if method == 'facebook':
+            user_info = requests.get('https://graph.facebook.com/me?fields=name,email&access_token='+authToken)
+            user_info_json = user_info.json()
+            return(user_info_json)
+        
+        if method == 'google':
+            user_info = requests.get('https://www.googleapis.com/oauth2/v1/userinfo?access_token='+authToken)
+            user_info_json = user_info.json()
+            return(user_info_json)
+
+        if method != 'google' and method != 'facebook':
+            return('Could not authenticate')
+        
+
+    def user_login(self, authToken, userEmail, method):
+        if not authToken:
+            return("Could not authenticate")
+        if not userEmail:
+            return("Could not authenticate")
+
+        db_query = self.connect_to_db.cursor()
+        db_query.execute("""SELECT * FROM zatiq_users WHERE auth_token = %s""", [authToken])
+        
+        response = db_query.fetchall()
+
+        if len(response) > 0:
+            user_info = self.get_user_info(authToken, method)
+            has_set_preferences = response[0][3]
+            user_name = user_info['name']
+            user_email = user_info['email']
+            api_token = response[0][5]
+            return(has_set_preferences, user_name, user_email, api_token)
+        else:
+            return("Could not authenticate")
+
+    def user_register(self, authToken, method):
+        if not authToken:
+            return("Could not authenticate")
+
+        db_query = self.connect_to_db.cursor()
+        db_query.execute("""SELECT * FROM zatiq_users WHERE auth_token = %s""", [authToken])
+
+        response = db_query.fetchall()
+        if len(response) > 0:
+            self.user_login(authToken, response[0][1], method)
+        else:
+            register_user = self.connect_to_db.cursor()
+            user_info = self.get_user_info(authToken, method)
+            user_id = user_info['id']
+            user_email = user_info['email']
+            user_name = user_info['name']
+            api_token = self.generate_zatiq_api_token()
+
+            if method == 'google':
+                register_user.execute("""INSERT INTO zatiq_users (user_email, user_name, auth_token, zatiq_token, google_id) VALUES (%s, %s, %s, %s, %s)""", (
+                    user_email, user_name, authToken, api_token, user_id))
+
+            if method == 'facebook':
+                register_user.execute("""INSERT INTO zatiq_users (user_email, user_name, auth_token, zatiq_token, facebook_id) VALUES (%s, %s, %s, %s, %s)""", (
+                    user_email, user_name, authToken, api_token, user_id))
+            
