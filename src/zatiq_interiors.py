2816744524a80fe1ad2ec3afe3e51e6d4bf57d7b
Hasan Jafri <hasan.jafri7@gmail.com>
2018-04-14 20:23:31 -0400

    Update businesses mongodb client

--- src/zatiq_interiors.py
+++ src/zatiq_interiors.py
@@ -4,6 +4,6 @@ import datetime
 
 class Zatiq_Interiors(Document):
     restaurant_id = ReferenceField(Zatiq_Businesses, reverse_delete_rule=CASCADE)
-    image = ListField(StringField())
-    image_aspect_ratio = ListField(StringField())
+    image = StringField(required=True)
+    image_aspect_ratio = StringField(required=True)
     date_created = DateTimeField(default=datetime.datetime.utcnow)
