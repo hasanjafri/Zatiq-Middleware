c2124ef06a4eb39ab1d4340498f03f381857bd76
Hasan Jafri <hasan.jafri7@gmail.com>
2018-04-17 23:26:33 -0400

    update user register

--- src/zatiq_user_preferences.py
+++ src/zatiq_user_preferences.py
@@ -3,13 +3,13 @@ from zatiq_meat_types import Zatiq_Meat_Types
 from zatiq_sea_food_types import Zatiq_Sea_Food_Types
 
 class Zatiq_User_Preferences(EmbeddedDocument):
-    halal = BooleanField(default=False)
-    spicy = BooleanField(default=True)
-    kosher = BooleanField(default=False)
-    healthy = BooleanField(default=False)
-    vegan = BooleanField(default=False)
-    vegetarian = BooleanField(default=False)
-    gluten_free = BooleanField(default=False)
-    nuts_allergy = BooleanField(default=True)
-    lactose_intolerant = BooleanField(default=False)
+    halal = BooleanField(required=True)
+    spicy = BooleanField(required=True)
+    kosher = BooleanField(required=True)
+    healthy = BooleanField(required=True)
+    vegan = BooleanField(required=True)
+    vegetarian = BooleanField(required=True)
+    gluten_free = BooleanField(required=True)
+    nuts_allergy = BooleanField(required=True)
+    lactose_intolerant = BooleanField(required=True)
 
