ee94cc9af7b5fb7073c539208f1436d8908d54ef
Hasan Jafri <hasan.jafri7@gmail.com>
2018-05-09 05:48:45 +0000

    update mongodb remote connection

--- src/application.py
+++ src/application.py
@@ -6,7 +6,7 @@ from zatiq_food_items_mongodb_client import ZatiqFoodItemsMongoDBClient
 from mongoengine import *
 
 application = Flask(__name__)
-connect('zatiq_database', host='165.227.43.65')
+connect('zatiq_database', host='165.227.43.65', username='zatiqadmin', password='zatiqserver')
 
 timely_meals = ['breakfast', 'brunch', 'lunch', 'dinner']
 cuisine_types = ['canadian', 'caribbean', 'chinese', 'dessert', 'fast_food', 'fine_food', 'gluten_free', 'greek', 'halal', 'healthy',
