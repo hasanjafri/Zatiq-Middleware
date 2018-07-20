7e62a233eb02fc6c1699ca6aedf1c60f695c0103
Hasan Jafri <hasan.jafri7@gmail.com>
2018-07-20 01:10:25 -0400

    convert byte array to string

--- src/application.py
+++ src/application.py
@@ -62,7 +62,7 @@ def add_zatiq_deal():
             error = "No image file was selected"
 
         if file and allowed_file(file.filename):
-            b64_img = base64.b64encode(file.read())
+            b64_img = base64.b64decode(file.read())
             res = zatiq_deals_client.save_image_to_db(b64_img.decode(), food_item_id)
             if isinstance(res, dict):
                 response = res
